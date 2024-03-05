4c507c6a6c41838729937ce719459276ad56c1e1
seyed <mohamad.khoshnava@gmail.com>
2024-03-05 10:35:44 +0330

    up

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = process.env.PARROT_PORT || 3000;
+const port = 80;
 server.listen(port, err => {
   if (err) throw err;
   console.log(`Listening on localhost:${port}`);
